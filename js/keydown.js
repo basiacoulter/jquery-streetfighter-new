ece55c0c7cfc7dd8abbc1bb4d2892942c8e2107c
basiacoulter <hello@basiacoulterdesign.com>
2014-10-20 09:06:21 -0400

    keydown

--- js/keydown.js
+++ js/keydown.js
@@ -1,8 +1,3 @@
-	/*.keydown(function(e) {
-		if(e.keyCode == 88) {
-			$('.ryu-cool').show();
-		}	
-	});*/
 	$(document).ready(function() {
 		$('.ryu').keydown(function(){
 			if(e.keyCode == 88) {
